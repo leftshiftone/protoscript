1632b3ae75941c79ee7a8ecba7dd02a950a0d8b1
W <manuel.weixler@leftshift.one>
2019-05-24 09:54:43 +0200

    (SmtpContentSpec)Added linebreak

--- protoscript-smtp/src/main/kotlin/protoscript/smtp/spec/SmtpContentSpec.kt
+++ protoscript-smtp/src/main/kotlin/protoscript/smtp/spec/SmtpContentSpec.kt
@@ -5,6 +5,7 @@ class SmtpContentSpec(private val builder:StringBuilder) {
     fun headline(text:String) = builder.append("<h1>$text</h1>")
     fun bold(text:String) = builder.append("<b>$text</b>")
     fun text(text:String) = builder.append("<span>$text</span>")
+    fun linebreak() = builder.append("<br>")
     fun paragraph(config: SmtpContentSpec.() -> Unit) {
         builder.append("<p>")
         SmtpContentSpec(builder).apply(config)
