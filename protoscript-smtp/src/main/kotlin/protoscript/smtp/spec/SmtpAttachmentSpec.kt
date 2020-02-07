4f98a933ed9e6146fc285dbdad1a5e008eedeb6b
christian <christian.weber@leftshift.one>
2020-02-07 07:20:06 +0100

    support for ms office attachments added

--- protoscript-smtp/src/main/kotlin/protoscript/smtp/spec/SmtpAttachmentSpec.kt
+++ protoscript-smtp/src/main/kotlin/protoscript/smtp/spec/SmtpAttachmentSpec.kt
@@ -7,12 +7,33 @@ import javax.mail.util.ByteArrayDataSource
 
 class SmtpAttachmentSpec(private val multipart: MimeMultipart) {
 
-    fun jpg(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "image/jpg", "${name}.jpg"))
-    fun xml(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "application/xml", "${name}.xml"))
-    fun svg(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "image/svg+xml", "${name}.svg"))
-    fun png(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "image/png", "${name}.png"))
-    fun pdf(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "application/pdf", "${name}.pdf"))
-    fun zip(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, "application/zip", "${name}.zip"))
+    companion object {
+        const val MIMETYPE_JPG = "image/jpg"
+        const val MIMETYPE_XML = "application/xml"
+        const val MIMETYPE_SVG = "image/svg+xml"
+        const val MIMETYPE_PNG = "image/png"
+        const val MIMETYPE_PDF = "application/pdf"
+        const val MIMETYPE_ZIP = "application/zip"
+        const val MIMETYPE_DOC = "application/msword"
+        const val MIMETYPE_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        const val MIMETYPE_XLS = "application/msexcel"
+        const val MIMETYPE_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        const val MIMETYPE_PPT = "application/mspowerpoint"
+        const val MIMETYPE_PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    }
+
+    fun jpg(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_JPG, "${name}.jpg"))
+    fun xml(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_XML, "${name}.xml"))
+    fun svg(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_SVG, "${name}.svg"))
+    fun png(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_PNG, "${name}.png"))
+    fun pdf(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_PDF, "${name}.pdf"))
+    fun zip(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_ZIP, "${name}.zip"))
+    fun doc(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_DOC, "${name}.doc"))
+    fun docx(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_DOCX, "${name}.docx"))
+    fun xls(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_XLS, "${name}.xls"))
+    fun xlsx(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_XLSX, "${name}.xlsx"))
+    fun ppt(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_PPT, "${name}.ppt"))
+    fun pptx(bytes: ByteArray, name: String) = multipart.addBodyPart(toMimeBodyPart(bytes, MIMETYPE_PPTX, "${name}.pptx"))
 
     private fun toMimeBodyPart(bytes: ByteArray, mimeType: String, name: String): MimeBodyPart {
         val part = MimeBodyPart()
